a62de069105e4431dcbb098c132f7c2fb67d0c25
Ohue James <ohuejames101@gmail.com>
2025-05-26 08:22:41 +0100

    package.json changes

--- BE/menu_service/server.js
+++ BE/menu_service/server.js
@@ -717,5 +717,5 @@ app.use(adminUploadRoutes);
 //   console.log("🧠 WebSocket Server running on port 3000")
 // );
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
