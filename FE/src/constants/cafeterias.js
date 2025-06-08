945240e912dd4f4b0647c123dbf0aefd030bd5dc
Ohue James <ohuejames101@gmail.com>
2025-06-08 15:58:03 +0100

    Re-add FE folder as regular directory

--- FE/src/constants/cafeterias.js
+++
@@ -1,7 +0,0 @@
-export const cafeterias = [
-  { id: 1, name: "Cafeteria 1" },
-  { id: 2, name: "Smoothie Shack" },
-  { id: 3, name: "Cafeteria 2" },
-  { id: 4, name: "Med Cafeteria" },
-  { id: 5, name: "Seasons Deli" },
-];
--- FE/src/utils/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-// export const socket = io("http://localhost:3000"); // or your IP
-export const socket = io("https://cuisino.onrender.com");
-
-export const registerSocket = (userId, role = "user") => {
-  socket.emit("register", { userId, role });
-};
